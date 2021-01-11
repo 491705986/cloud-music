8e31f0a598f375c26ac7af6241aaf923779d7b2f
Benyuan.yan <491705986@qq.com>
2021-01-11 21:55:49 +0800

    feat: 歌手页完成

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -19,6 +19,7 @@ function Recommend(props) {
     if (!recommendList.size) {
       getRecommendListDataDispatch()
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const bannerListJS = bannerList ? bannerList.toJS() : []
--- src/baseUI/scroll/index.js
+++ src/baseUI/scroll/index.js
@@ -1,7 +1,10 @@
-import React, { forwardRef, useState, useEffect, useRef, useImperativeHandle } from 'react'
+import React, { forwardRef, useState, useEffect, useRef, useImperativeHandle, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import BScroll from 'better-scroll'
 import styled from 'styled-components'
+import Loading from '../loading'
+import { debounce } from '../../api/utils'
+import LoadingV2 from '../loading-v2'
 
 const ScrollContainer = styled.div`
   width: 100%;
@@ -9,23 +12,44 @@ const ScrollContainer = styled.div`
   overflow: hidden;
 `
 
-const Scroll = forwardRef((props, ref) => {
-  const {
-    direction,
-    click,
-    refresh,
-    // pullUpLoading,
-    // pullDownLoading,
-    bounceTop,
-    bounceBottom,
-    pullUp,
-    pullDown,
-    onScroll
-  } = props
+const PullUpLoading = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 5px;
+  width: 60px;
+  height: 60px;
+  margin: auto;
+  z-index: 100;
+`
 
+export const PullDownLoading = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0px;
+  height: 30px;
+  margin: auto;
+  z-index: 100;
+`
+
+const Scroll = forwardRef((props, ref) => {
   const [bScroll, setBScroll] = useState()
+
   const scrollContaninerRef = useRef()
-  // 创建 better-scroll
+
+  const { direction, click, refresh, bounceTop, bounceBottom } = props
+
+  const { pullUp, pullDown, onScroll, pullUpLoading, pullDownLoading } = props
+
+  let pullUpDebounce = useMemo(() => {
+    return debounce(pullUp, 300)
+  }, [pullUp])
+
+  let pullDownDebounce = useMemo(() => {
+    return debounce(pullDown, 300)
+  }, [pullDown])
+
   useEffect(() => {
     const scroll = new BScroll(scrollContaninerRef.current, {
       scrollX: direction === 'horizental',
@@ -41,9 +65,9 @@ const Scroll = forwardRef((props, ref) => {
     return () => {
       setBScroll(null)
     }
+    // eslint-disable-next-line
   }, [])
 
-  // 给实例绑定 scroll 事件
   useEffect(() => {
     if (!bScroll || !onScroll) return
     bScroll.on('scroll', scroll => {
@@ -53,48 +77,46 @@ const Scroll = forwardRef((props, ref) => {
       bScroll.off('scroll')
     }
   }, [onScroll, bScroll])
-  // 每次重新渲染都要刷新实例，防止无法滑动
-  useEffect(() => {
-    if (refresh && bScroll) {
-      bScroll.refresh()
-    }
-  })
+
   useEffect(() => {
     if (!bScroll || !pullUp) return
     bScroll.on('scrollEnd', () => {
-      // 判断是否滑动到了底部
+      //判断是否滑动到了底部
       if (bScroll.y <= bScroll.maxScrollY + 100) {
-        pullUp()
+        pullUpDebounce()
       }
     })
     return () => {
       bScroll.off('scrollEnd')
     }
-  }, [pullUp, bScroll])
+  }, [pullUpDebounce, pullUp, bScroll])
 
   useEffect(() => {
     if (!bScroll || !pullDown) return
     bScroll.on('touchEnd', pos => {
-      // 判断用户的下拉动作
+      //判断用户的下拉动作
       if (pos.y > 50) {
-        pullDown()
+        pullDownDebounce()
       }
     })
     return () => {
       bScroll.off('touchEnd')
     }
-  }, [pullDown, bScroll])
+  }, [pullDownDebounce, pullDown, bScroll])
 
-  // 一般和 forwardRef 一起使用，ref 已经在 forWardRef 中默认传入
+  useEffect(() => {
+    if (refresh && bScroll) {
+      bScroll.refresh()
+    }
+  })
+
   useImperativeHandle(ref, () => ({
-    // 给外界暴露 refresh 方法
     refresh() {
       if (bScroll) {
         bScroll.refresh()
         bScroll.scrollTo(0, 0)
       }
     },
-    // 给外界暴露 getBScroll 方法，提供 bs 实例
     getBScroll() {
       if (bScroll) {
         return bScroll
@@ -102,7 +124,21 @@ const Scroll = forwardRef((props, ref) => {
     }
   }))
 
-  return <ScrollContainer ref={scrollContaninerRef}>{props.children}</ScrollContainer>
+  const PullUpdisplayStyle = pullUpLoading ? { display: '' } : { display: 'none' }
+  const PullDowndisplayStyle = pullDownLoading ? { display: '' } : { display: 'none' }
+  return (
+    <ScrollContainer ref={scrollContaninerRef}>
+      {props.children}
+      {/* 滑到底部加载动画 */}
+      <PullUpLoading style={PullUpdisplayStyle}>
+        <Loading></Loading>
+      </PullUpLoading>
+      {/* 顶部下拉刷新动画 */}
+      <PullDownLoading style={PullDowndisplayStyle}>
+        <LoadingV2></LoadingV2>
+      </PullDownLoading>
+    </ScrollContainer>
+  )
 })
 
 Scroll.defaultProps = {
@@ -119,16 +155,15 @@ Scroll.defaultProps = {
 }
 
 Scroll.propTypes = {
-  irection: PropTypes.oneOf(['vertical', 'horizental']), // 滚动的方向
-  click: PropTypes.bool, // 是否支持点击
-  refresh: PropTypes.bool, // 是否刷新
-  onScroll: PropTypes.func, // 滑动触发的回调函数
-  pullUp: PropTypes.func, // 上拉加载逻辑
-  pullDown: PropTypes.func, // 下拉加载逻辑
-  pullUpLoading: PropTypes.bool, // 是否显示上拉 loading 动画
-  pullDownLoading: PropTypes.bool, // 是否显示下拉 loading 动画
-  bounceTop: PropTypes.bool, // 是否支持向上吸顶
-  bounceBottom: PropTypes.bool // 是否支持向下吸底
+  direction: PropTypes.oneOf(['vertical', 'horizental']),
+  refresh: PropTypes.bool,
+  onScroll: PropTypes.func,
+  pullUp: PropTypes.func,
+  pullDown: PropTypes.func,
+  pullUpLoading: PropTypes.bool,
+  pullDownLoading: PropTypes.bool,
+  bounceTop: PropTypes.bool, //是否支持向上吸顶
+  bounceBottom: PropTypes.bool //是否支持向上吸顶
 }
 
 export default Scroll
