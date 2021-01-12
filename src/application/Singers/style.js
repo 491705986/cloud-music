fcae926d5480a695857c98c6a54d4ff2df666639
Benyuan.yan <491705986@qq.com>
2021-01-12 20:09:24 +0800

    fix: 排行榜完成

--- src/application/Singers/style.js
+++ src/application/Singers/style.js
@@ -9,6 +9,7 @@ export const NavContainer = styled.div`
   padding: 5px;
   overflow: hidden;
 `
+
 export const ListContainer = styled.div`
   position: fixed;
   top: 160px;
@@ -50,4 +51,14 @@ export const ListItem = styled.div`
     color: ${style['font-color-desc']};
     font-weight: 500;
   }
+`
+export const EnterLoading = styled.div`
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  width: 100px;
+  height: 100px;
+  margin: auto;
 `
